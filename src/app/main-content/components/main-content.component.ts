41ef0fed77b382334eeb7b682848b7e591a8f766
mihaiandreineacsu <106650061+mihaiandreineacsu@users.noreply.github.com>
2024-06-03 08:30:09 +0200

    create left and right panels togglers

--- src/app/main-content/components/main-content.component.ts
+++ src/app/main-content/components/main-content.component.ts
@@ -1,6 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { AuthProcessService } from 'ngx-auth-firebaseui';
 import { MainContentModule } from '../main-content.module';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-main-content',
@@ -9,6 +11,22 @@ import { MainContentModule } from '../main-content.module';
   standalone: true,
   imports: [MainContentModule]
 })
-export class MainContentComponent {
+export class MainContentComponent implements OnInit, OnDestroy {
   public auth = inject(AuthProcessService);
+  private breakpointObserver = inject(BreakpointObserver);
+  private breakpointSub!: Subscription;
+  sidesMode: "side" | "over" = "side";
+
+  ngOnInit(): void {
+    this.breakpointSub?.unsubscribe();
+    this.breakpointSub = this.breakpointObserver
+      .observe([Breakpoints.Large])
+      .subscribe(result => {
+        this.sidesMode = result.matches ? "side" : "over";
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.breakpointSub?.unsubscribe();
+  }
 }
