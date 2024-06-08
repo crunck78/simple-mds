390ec4ae6398694378325330511ad8f4b2553e51
mihaiandreineacsu <106650061+mihaiandreineacsu@users.noreply.github.com>
2024-06-08 11:30:48 +0200

    message view place logged in user messages on right side responsive change

--- src/app/main-content/components/message-view/message-view/message-view.component.ts
+++ src/app/main-content/components/message-view/message-view/message-view.component.ts
@@ -2,9 +2,10 @@ import { Component, Input, OnChanges, OnDestroy, inject } from '@angular/core';
 import { Message } from 'src/app/shared/models/message.class';
 import { UsersService } from 'src/app/shared/services/users/users.service';
 import { User } from 'src/app/shared/models/user.class';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from 'src/app/shared/modules/material.module';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-message-view',
@@ -17,17 +18,22 @@ export class MessageViewComponent implements OnChanges, OnDestroy {
 
   @Input() message: Message | undefined;
   author: User | undefined;
+  signInUser!: firebase.User | null;
+  singInUserSub!: Subscription;
   authorSub!: Subscription;
   private usersService = inject(UsersService);
 
   ngOnChanges(): void {
+    this.singInUserSub?.unsubscribe();
+    this.singInUserSub = this.usersService.getSignedInUser$()
+      .subscribe(change => this.signInUser = change);
     this.authorSub?.unsubscribe();
     this.authorSub = this.usersService.getUser$(this.message?.author as string)
       .subscribe(change => this.author = change as User);
-    console.log(this.message);
   }
 
   ngOnDestroy(): void {
+    this.singInUserSub?.unsubscribe();
     this.authorSub?.unsubscribe();
   }
 
