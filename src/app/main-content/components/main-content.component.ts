17b1620563ba62426a6596193aff084c6ed75332
mihaiandreineacsu <106650061+mihaiandreineacsu@users.noreply.github.com>
2024-06-08 11:26:45 +0200

    add resize right content

--- src/app/main-content/components/main-content.component.ts
+++ src/app/main-content/components/main-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit, inject } from '@angular/core';
 import { AuthProcessService } from 'ngx-auth-firebaseui';
 import { MainContentModule } from '../main-content.module';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
@@ -17,6 +17,13 @@ export class MainContentComponent implements OnInit, OnDestroy {
   private breakpointSub!: Subscription;
   sidesMode: "side" | "over" = "side";
 
+  drawerWidth = 300;
+  isResizing = false;
+  startX!: number;
+  startWidth!: number;
+  readonly minDrawerWidth = 300;
+  maxDrawerWidth = window.innerWidth * 0.9; // 90vw
+
   ngOnInit(): void {
     this.breakpointSub?.unsubscribe();
     this.breakpointSub = this.breakpointObserver
@@ -28,5 +35,32 @@ export class MainContentComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.breakpointSub?.unsubscribe();
+  }
+
+  onResizeStart(event: MouseEvent): void {
+    this.isResizing = true;
+    this.startX = event.clientX;
+    this.startWidth = this.drawerWidth;
+    event.preventDefault();
+  }
+
+  @HostListener('window:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent): void {
+    if (this.isResizing) {
+      const offset = event.clientX - this.startX;
+      this.drawerWidth = Math.max(this.minDrawerWidth, Math.min(this.startWidth - offset, this.maxDrawerWidth));
+    }
+  }
+
+  @HostListener('window:mouseup', ['$event'])
+  onMouseUp(event: MouseEvent): void {
+    if (this.isResizing) {
+      this.isResizing = false;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event: Event): void {
+    this.maxDrawerWidth = window.innerWidth * 0.9; // Update max width on window resize
   }
 }
